c819de78398510701129350bf69a8ac8463c2edf
Hemang Patel <136294289+Hemang-develops@users.noreply.github.com>
2024-09-16 12:55:23 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,12 @@
-# build_files.sh
+#!/bin/bash
+
+# Set up Python environment
+python -m venv venv
+source venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+
+# Run Django build commands or any other setup steps
+python manage.py collectstatic --noinput
+python manage.py migrate
